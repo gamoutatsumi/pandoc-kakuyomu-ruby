73e217d08c08418b68e47217b087e4e766f03c12
Gamou-T <47162587+Gamou-T@users.noreply.github.com>
2019-06-19 18:12:03 +0900

    記号に対応（暫定）

--- ruby_kenten.py
+++ ruby_kenten.py
@@ -5,7 +5,7 @@ import regex
 
 def ruby(key, val, fmt, meta):
     if key == 'Str':
-        if regex.search(r'(｜((?!《)(\p{Hiragana}|\p{Katakana}|\p{Han}|ー)+?))|(\p{Han}+?)《', val):
+        if regex.search(r'(｜((\p{Hiragana}|\p{Katakana}|\p{Han}|ー|\p{P})+?))|(\p{Han}+?)《', val):
             for matchedVals in regex.findall(r'(?:(?:｜(?:\p{Hiragana}|\p{Katakana}|\p{Han}|ー)+?)|(?:\p{Han}+?))《.*?》', val):
                 base = regex.search(r'(((?<=｜)(.*?)(?=《))|(\p{Han}*?(?=《)))', matchedVals).groups(1)[0]
                 ruby = regex.search(r'((?<=《)(.*?)(?=》))', matchedVals).groups(1)[1]
@@ -23,7 +23,7 @@ def ruby(key, val, fmt, meta):
                     filteredStr = r'<ruby><rb>%s</rb><rp>《</rp><rt>%s</rt><rp>》</rp></ruby>' % (base,ruby)
                 val = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, val)
         if regex.search(r'《《', val):
-            for matchedVals in regex.findall(r'《《(?:\p{Hiragana}|\p{Katakana}|\p{Han}|ー)+?》》', val):
+            for matchedVals in regex.findall(r'《《(?:\p{Hiragana}|\p{Katakana}|\p{Han}|\p{P}|ー)+?》》', val):
                 base = regex.search(r'《《(.+?)》》', matchedVals).groups(0)[0]
                 if fmt == 'latex':
                     filteredStr = r'\\kenten{%s}' % (base)
@@ -33,6 +33,8 @@ def ruby(key, val, fmt, meta):
                         kenten += r'・'
                     filteredStr = r'<ruby><rb>%s</rb><rp>《</rp><rt>%s</rt><rp>》</rp></ruby>' % (base,kenten)
                 val = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, val)
+        if regex.search(r'｜《', val):
+            val = regex.sub(r'｜《', r'《', val)
         if fmt == 'latex':
             return RawInline('tex', r'%s' %val)
         elif fmt == 'html' or fmt == 'html5' or fmt == 'epub' or fmt == 'epub3':
