96dd590cf1898f457d4e4316db91a428706802dd
Gamou-T <47162587+Gamou-T@users.noreply.github.com>
2019-06-22 20:55:32 +0900

    PDFに変換した際の不具合を解消

--- ruby_kenten.py
+++ ruby_kenten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
-from pandocfilters import toJSONFilter, Str, RawInline, RawBlock
+from pandocfilters import toJSONFilter, Str, RawInline, RawBlock, Header
 import regex
 
 def ruby(key, val, fmt, meta):
-    if key == 'Header' and fmt == 'latex':
-        headerLevel = val[0]
-        val = val[1][0]
-        val = r'\section{%s}' %val
-        for i in range(headerLevel - 1):
-            val = regex.sub(r'^.', r'\sub', val)
-        return RawBlock('tex', val)
+    if key == 'Header':
+        val[1][0] = val[2][0]['c']
+        return Header(val[0], val[1], val[2])
     if key != 'Str':
         return
-    for matchedVals in regex.findall(r'(?:(?:｜(?:\p{Hiragana}|\p{Katakana}|\p{Han}|ー)+?)|(?:\p{Han}+?))《.*?》', val):
+    filteredVal = val
+    matched = False
+    for matchedVals in regex.findall(r'(?:(?:｜(?:\p{Hiragana}|\p{Katakana}|\p{Han}|ー)+?)|(?:\p{Han}+?))《.*?》', filteredVal):
         base = regex.search(r'(((?<=｜)(.*?)(?=《))|(\p{Han}*?(?=《)))', matchedVals).groups(1)[0]
         ruby = regex.search(r'((?<=《)(.*?)(?=》))', matchedVals).groups(1)[1]
         filteredRuby = regex.search(r'^((.*?)(?=｜))', ruby)[0] if regex.search(r'(.*)?｜(?!.*《)(?!.*｜)', ruby) else ruby
@@ -27,26 +25,34 @@ def ruby(key, val, fmt, meta):
             filteredStr = r'\\ruby{%s}{%s}' % (base,ruby)
         elif fmt == 'html' or fmt == 'html5' or fmt == 'epub' or fmt == 'epub3':
             filteredStr = r'<ruby><rb>%s</rb><rp>《</rp><rt>%s</rt><rp>》</rp></ruby>' % (base,ruby)
-        val = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, val)
-
-    for matchedVals in regex.findall(r'《《(?:\p{Hiragana}|\p{Katakana}|\p{Han}|\p{P}|ー)+?》》', val):
+        filteredVal = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, filteredVal)
+        matched = True
+    for matchedVals in regex.findall(r'《《(?:\p{Hiragana}|\p{Katakana}|\p{Han}|\p{P}|ー)+?》》', filteredVal):
         base = regex.search(r'《《(.+?)》》', matchedVals).groups(0)[0]
         if fmt == 'latex':
-            filteredStr = r'\\kenten{%s}' % (base)
+            filteredStr = r'\\kenten{%s}' % base
         elif fmt == 'html' or 'html5' or fmt == 'epub' or fmt == 'epub3':
             kenten = ''
             for kentenCount in base:
                 kenten += r'・'
             filteredStr = r'<ruby><rb>%s</rb><rp>《</rp><rt>%s</rt><rp>》</rp></ruby>' % (base,kenten)
-        val = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, val)
+        filteredVal = regex.sub(r'%s' % matchedVals, r'%s' % filteredStr, filteredVal)
+        mathced = True
 
-    if regex.search(r'｜《', val):
-        val = regex.sub(r'｜《', r'《', val)
+    if regex.search(r'｜《', filteredVal):
+        filteredVal = regex.sub(r'｜《', r'《', filteredVal)
+        if matched is False:
+            matched = True
 
-    if fmt == 'latex':
-        return RawInline('tex', r'%s' %val)
-    elif fmt == 'html' or fmt == 'html5' or fmt == 'epub' or fmt == 'epub3':
-        return RawInline('html', r'%s' %val)
+    if matched is True:
+        if fmt == 'latex':
+            return RawInline('latex',r'%s' %filteredVal)
+        elif fmt == 'html' or fmt == 'html5' or fmt == 'epub' or fmt == 'epub3':
+            return RawInline('html', r'%s' %filteredVal)
+    else:
+        return
+    
+
 
 
 if __name__ == '__main__':
